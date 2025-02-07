41cf71d1357f13377817de9fb643820029dc2032
Sarav <smuruhesan@paloaltonetworks.com>
2025-02-07 16:31:28 +0800

    updated nav and tools page with demo links

--- nav.php
+++ nav.php
@@ -12,12 +12,10 @@
         <ul>
           <li><a href="index.php" class="active">Home</a></li>
           <li><a href="tools.php">Tools</a></li>
-
-          <!--  DISABLING THE PLAYBOOK OPTION
           <li class="dropdown"><a href="playbook.php"><span>Playbooks</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
               <li><a href="playbook.php?cat=cloudsec">Cloud Security</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+              <!-- <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
                 <ul>
                   <li><a href="#">Deep Dropdown 1</a></li>
                   <li><a href="#">Deep Dropdown 2</a></li>
@@ -25,13 +23,12 @@
                   <li><a href="#">Deep Dropdown 4</a></li>
                   <li><a href="#">Deep Dropdown 5</a></li>
                 </ul>
-              </li> 
+              </li> --> 
               <li><a href="playbook.php?cat=runtimesec">Runtime Security</a></li>
               <li><a href="playbook.php?cat=appsec">Application Security</a></li>
               <li><a href="playbook.php?cat=process">Process</a></li>
             </ul>
           </li>
-          -->
           <li><a href="reference.php">Quick References</a></li>
           <li><a href="sample.php">Quick Links</a></li>
         </ul>
